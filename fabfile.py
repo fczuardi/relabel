253fb16043b8371c678ab7f7a5cd41a1f3570d4e
Jonas Galvez <jonas@celine.(none)>
2010-03-06 07:00:43 -0300

    Made fab start_redis actually work, added redis.conf.orig so it can always replace on the original params. Also made Git ignore ext/redis/redis.conf since that's deployment-specific and handled by fab start_redis.

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,9 @@ import os, sys
 RELABEL_ROOT = os.path.dirname(__file__)
 sys.path.append(os.path.join(RELABEL_ROOT, 'lib'))
 
-from relabel import Relabel
+from fabric.api import *
+
+# from relabel import Relabel
   
 def web():  
   http_server = tornado.httpserver.HTTPServer(Relabel.application)
@@ -17,7 +19,7 @@ def start_redis():
     os.makedirs(os.path.join(RELABEL_ROOT, 'data'))
     
   # define redis configuration parameters
-  redis_pid_file = os.path(RELABEL_ROOT, 'run', 'redis.pid')
+  redis_pid_file = os.path.join(RELABEL_ROOT, 'run', 'redis.pid')
   redis_db_file = os.path.join(RELABEL_ROOT, 'data', 'relabel.rdb')
   redis_options = {
     'pidfile /var/run/redis.pid': 'pidfile %s' % redis_pid_file,
@@ -25,19 +27,22 @@ def start_redis():
     'dbfilename dump.rdb': 'dbfilename %s' % redis_db_file
   }
 
-  # write redis configuration parameters
-  redis_config = open(os.path.join(RELABEL_ROOT, 'ext', 'redis', 'redis.conf'), 'w')
+  # read redis default configuration parameters
+  redis_config = open(os.path.join(RELABEL_ROOT, 'ext', 'redis', 'redis.conf.orig'), 'r')
   redis_config_raw = redis_config.read()
   redis_config.close()
-  redis_config = open(os.path.join(RELABEL_ROOT, 'ext', 'redis', 'redis.conf'), 'w')
+
+  # write redis app-specific configuration parameters
+  redis_config_path = os.path.join(RELABEL_ROOT, 'ext', 'redis', 'redis.conf')
+  redis_config = open(redis_config_path, 'w')
   for defaultv, newv in redis_options.items():
     redis_config_raw = redis_config_raw.replace(defaultv, newv)
   redis_config.write(redis_config_raw)
   redis_config.close()
 
   # start redis server
-  local(os.path(RELABEL_ROOT, 'ext', 'redis-server'))
+  local("%s %s" % (os.path.join(RELABEL_ROOT, 'ext', 'redis', 'redis-server'), redis_config_path))
   
 def kill_redis():
   # start redis server
-  local("kill `cat %s`" % os.path(RELABEL_ROOT, 'run', 'redis.pid'))
+  local("kill `cat %s`" % os.path.join(RELABEL_ROOT, 'run', 'redis.pid'))
